2320ccf7486bab794fc06ee41d0e5a42cc781ed9
YesbolBagyt <yeabolbagyt@gmail.com>
2025-01-10 13:26:43 +0500

    Zookeeper.java

--- src/Zookeeper.java
+++ src/Zookeeper.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Zookeeper {
     private String name;
     private int employeeId;
@@ -34,8 +40,119 @@ public class Zookeeper {
     }
 
     public void displayInfo() {
-        System.out.println("Zookeeper Name: " + name);
-        System.out.println("Employee ID: " + employeeId);
-        System.out.println("Years of Experience: " + yearsOfExperience);
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Zookeeper { " +
+                "Name='" + name + '\'' +
+                ", Employee ID=" + employeeId +
+                ", Years of Experience=" + yearsOfExperience +
+                " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Zookeeper zookeeper = (Zookeeper) o;
+        return employeeId == zookeeper.employeeId &&
+                yearsOfExperience == zookeeper.yearsOfExperience &&
+                Objects.equals(name, zookeeper.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, employeeId, yearsOfExperience);
+    }
+}
+
+class SeniorZookeeper extends Zookeeper {
+    private String specialty;
+
+    public SeniorZookeeper(String name, int employeeId, int yearsOfExperience, String specialty) {
+        super(name, employeeId, yearsOfExperience);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Specialty: " + specialty);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Specialty='" + specialty + '\'' + " }";
+    }
+}
+
+class ZookeeperDataPool {
+    private List<Zookeeper> zookeepers;
+
+    public ZookeeperDataPool() {
+        this.zookeepers = new ArrayList<>();
+    }
+
+    public void addZookeeper(Zookeeper zookeeper) {
+        zookeepers.add(zookeeper);
+    }
+
+    public void displayAllZookeepers() {
+        zookeepers.forEach(Zookeeper::displayInfo);
+    }
+
+    public List<Zookeeper> filterByExperience(int minYears) {
+        return zookeepers.stream()
+                .filter(zookeeper -> zookeeper.getYearsOfExperience() >= minYears)
+                .collect(Collectors.toList());
+    }
+
+    public Zookeeper searchByName(String name) {
+        return zookeepers.stream()
+                .filter(zookeeper -> zookeeper.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Zookeeper> sortByExperience() {
+        return zookeepers.stream()
+                .sorted(Comparator.comparingInt(Zookeeper::getYearsOfExperience).reversed())
+                .collect(Collectors.toList());
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        ZookeeperDataPool dataPool = new ZookeeperDataPool();
+
+        Zookeeper zookeeper1 = new Zookeeper("Alice", 101, 5);
+        SeniorZookeeper zookeeper2 = new SeniorZookeeper("Bob", 102, 10, "Large Mammals");
+        SeniorZookeeper zookeeper3 = new SeniorZookeeper("Charlie", 103, 15, "Reptiles");
+
+        dataPool.addZookeeper(zookeeper1);
+        dataPool.addZookeeper(zookeeper2);
+        dataPool.addZookeeper(zookeeper3);
+
+        System.out.println("All Zookeepers:");
+        dataPool.displayAllZookeepers();
+
+        System.out.println("\nFilter by Experience (>= 10 years):");
+        dataPool.filterByExperience(10).forEach(System.out::println);
+
+        System.out.println("\nSearch by Name (Bob):");
+        System.out.println(dataPool.searchByName("Bob"));
+
+        System.out.println("\nSort by Years of Experience:");
+        dataPool.sortByExperience().forEach(System.out::println);
     }
 }
